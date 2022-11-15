c4d9f1bf669019fbd332b1cb5d72edbaeaeaa54e
sorooshbagheri <sorooshbagheri@outlook.com>
2022-11-16 01:01:49 +0330

    switched to AlmasMovie Series

--- Iran-addon/addon.js
+++ Iran-addon/addon.js
@@ -1,123 +1,156 @@
 const { addonBuilder } = require("stremio-addon-sdk");
 
-const got = (...args) => import('got').then(({default: got}) => got(...args));
-const cheerio = require("cheerio")
-const fs = require('fs');
+const got = (...args) => import("got").then(({ default: got }) => got(...args));
+const cheerio = require("cheerio");
+const fs = require("fs");
 
-var lib = {}
+var lib = {};
 
 // import got from "got";
 // import * as cheerio from "cheerio";
 
-const loadLib = ()=>{
-  fs.readFile('./lib.json',(err, jsonString) => {
-    if (err) {
-      console.log("File read failed:", err);
-      return 
-    }
-    console.log('Library imported successfully.');
-    lib = JSON.parse(jsonString);
-    // console.log(lib);
-  })
-}
-loadLib()
+const loadLib = () => {
+    fs.readFile("./lib.json", (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err);
+            return;
+        }
+        console.log("Library imported successfully.");
+        lib = JSON.parse(jsonString);
+        // console.log(lib);
+    });
+};
+loadLib();
 
 // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
-  id: "community.DonyayeSerial",
-  version: "0.0.3",
-  catalogs: [],
-  resources: ["stream"],
-  types: ["movie", "series"],
-  name: "Iranian Servers",
-  description: "To stream from Iranian urls",
-  idPrefixes: ["kitsu", "tt"],
+    id: "community.DonyayeSerial",
+    version: "0.0.3",
+    catalogs: [],
+    resources: ["stream"],
+    types: ["movie", "series"],
+    name: "Iranian Servers",
+    description: "To stream from Iranian urls",
+    idPrefixes: ["kitsu", "tt"],
 };
 const builder = new addonBuilder(manifest);
 
 builder.defineStreamHandler(({ type, id }) => {
-  console.log("request for streams: " + type + " " + id);
-  // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineStreamHandler.md
-  [prefix, series, episode] = id.split(":");
+    console.log("request for streams: " + type + " " + id);
+    // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineStreamHandler.md
+    [prefix, series, episode] = id.split(":");
 
-  return getStreams(id).then((streams) => ({ streams }));
+    // return getStreams(id).then((streams) => ({ streams }));
+    return getAlmasMovieStreams(id).then((streams) => ({ streams }));
 });
 
 module.exports = builder.getInterface();
 
 const getStreams = async function (id) {
-  let streams = [];
+    let streams = [];
 
-  // check the offline library
-  if (lib[id]){
-    streams = lib[id];
-    console.log("Retrieved from library:", streams);
-    return Promise.resolve(streams);
-  }
-
-  // Naruto Shippuden
-  [prefix, series, episode] = id.split(":");
-  if (prefix === "kitsu" && series === "1555") {
-    const baseURL = `http://dls2.top-movies2filmha.tk/DonyayeSerial/series/Naruto.Shippuuden/`;
-    const range = [
-      (Math.floor((episode - 1) / 50) * 50 + 1 + "").padStart(3, "0"),
-      (Math.floor((episode - 1) / 50) * 50 + 50 + "").padStart(3, "0"),
-    ];
-    streams = [
-      {
-        title: "1080p x265 WEBRip AnimeRG mkv",
-        url: `${baseURL}${range[0]}-${
-          range[1]
-        }/1080p.x265.WEBRip/Naruto.Shippuden.${episode.padStart(
-          3,
-          "0"
-        )}.1080p.x265.WEBRip.AnimeRG.DonyayeSerial.mkv`,
-        behaviorHints: {
-          bingeGroup: "Naruto.Shippuden.1080p.x265.WEBRip.AnimeRG.DonyayeSerial",
-        },
-      },
-      {
-        title: "720p x265 mkv",
-        url: `${baseURL}${range[0]}-${
-          range[1]
-        }/720p.x265/Naruto.Shippuden.${episode.padStart(
-          3,
-          "0"
-        )}.720p.x265.DonyayeSerial.mkv`,
-        behaviorHints: {
-          bingeGroup: "Naruto.Shippuden.720p.x265.DonyayeSerial",
-        },
-      },
-    ];
-  }
-
-  // The Simpsons
-  [series_id, season, episode] = id.split(":");
-  if (series_id === "tt0096697") {
-    const baseURL =
-      "https://img5.downloadha.com/hosein/Animation/October2019/The.Simpsons/";
-    if (season < 13) {
-      const res = await got(baseURL + `S${season.padStart(2, "0")}/`);
-      const $ = cheerio.load(res.body);
-      $("a").each((i, elem) => {
-        const link = elem.attribs.href;
-        if (
-          link.split(".")[2] ===
-          `S${season.padStart(2, "0")}E${episode.padStart(2, "0")}`
-        ) {
-          streams = [
-            {
-              title: "DVDRip-jlw mkv",
-              url: `${baseURL}S${season.padStart(2, "0")}/${link}`,
-              behaviorHints: {
-                bingeGroup: `The.Simpsons`,
-              },
-            },
-          ];
-        }
-      });
+    // check the offline library
+    if (lib[id]) {
+        streams = lib[id];
+        console.log("Retrieved from library:", streams);
+        return Promise.resolve(streams);
     }
+
+    // Naruto Shippuden
+    [prefix, series, episode] = id.split(":");
+    if (prefix === "kitsu" && series === "1555") {
+        const baseURL = `http://dls2.top-movies2filmha.tk/DonyayeSerial/series/Naruto.Shippuuden/`;
+        const range = [
+            (Math.floor((episode - 1) / 50) * 50 + 1 + "").padStart(3, "0"),
+            (Math.floor((episode - 1) / 50) * 50 + 50 + "").padStart(3, "0"),
+        ];
+        streams = [
+            {
+                title: "1080p x265 WEBRip AnimeRG mkv",
+                url: `${baseURL}${range[0]}-${
+                    range[1]
+                }/1080p.x265.WEBRip/Naruto.Shippuden.${episode.padStart(
+                    3,
+                    "0"
+                )}.1080p.x265.WEBRip.AnimeRG.DonyayeSerial.mkv`,
+                behaviorHints: {
+                    bingeGroup:
+                        "Naruto.Shippuden.1080p.x265.WEBRip.AnimeRG.DonyayeSerial",
+                },
+            },
+            {
+                title: "720p x265 mkv",
+                url: `${baseURL}${range[0]}-${
+                    range[1]
+                }/720p.x265/Naruto.Shippuden.${episode.padStart(
+                    3,
+                    "0"
+                )}.720p.x265.DonyayeSerial.mkv`,
+                behaviorHints: {
+                    bingeGroup: "Naruto.Shippuden.720p.x265.DonyayeSerial",
+                },
+            },
+        ];
+    }
+
+    // The Simpsons
+    [series_id, season, episode] = id.split(":");
+    if (series_id === "tt0096697") {
+        const baseURL =
+            "https://img5.downloadha.com/hosein/Animation/October2019/The.Simpsons/";
+        if (season < 13) {
+            const res = await got(baseURL + `S${season.padStart(2, "0")}/`);
+            const $ = cheerio.load(res.body);
+            $("a").each((i, elem) => {
+                const link = elem.attribs.href;
+                if (
+                    link.split(".")[2] ===
+                    `S${season.padStart(2, "0")}E${episode.padStart(2, "0")}`
+                ) {
+                    streams = [
+                        {
+                            title: "DVDRip-jlw mkv",
+                            url: `${baseURL}S${season.padStart(
+                                2,
+                                "0"
+                            )}/${link}`,
+                            behaviorHints: {
+                                bingeGroup: `The.Simpsons`,
+                            },
+                        },
+                    ];
+                }
+            });
+        }
+    }
+    console.log(streams);
+    return Promise.resolve(streams);
+};
+
+const getAlmasMovieStreams = async function (id) {
+  let streams = [];
+  [series_id, season, episode] = id.split(":");
+  const baseURL = "http://iamnotindangeriamthedanger.website/filmgir/?i=";
+  for (let q = 1; q < 2; q++) {
+      const res = await got(baseURL + `${series_id}&f=${season}&q=${q}`);
+      const $ = cheerio.load(res.body);
+      const title = $("div.mb-2").text();
+      if (title) {
+          console.log(title);
+          $("a").each((i, elem) => {
+              if (i  == (episode -1) * 2) {
+                  streams.push({
+                      title: title,
+                      url: `${elem.attribs.href}`,
+                      behaviorHints: {
+                          bingeGroup: series_id + " " + title,
+                      },
+                  });
+              }
+          });
+      }
   }
   console.log(streams);
   return Promise.resolve(streams);
+
 };
--- Iran-addon/test.js
+++ Iran-addon/test.js
@@ -2,73 +2,107 @@ const got = (...args) => import("got").then(({ default: got }) => got(...args));
 const cheerio = require("cheerio");
 
 const getStreams = async function (id) {
-  let streams = [];
+    let streams = [];
 
-  // Naruto Shippuden
-  [prefix, series, episode] = id.split(":");
-  if (prefix === "kitsu" && series === "1555") {
-    const baseURL = `http://dls2.top-movies2filmha.tk/DonyayeSerial/series/Naruto.Shippuuden/`;
-    const range = [
-      (Math.floor((episode - 1) / 50) * 50 + 1 + "").padStart(3, "0"),
-      (Math.floor((episode - 1) / 50) * 50 + 50 + "").padStart(3, "0"),
-    ];
-    streams = [
-      {
-        title: "1080p x265 WEBRip AnimeRG mkv",
-        url: `${baseURL}${range[0]}-${
-          range[1]
-        }/1080p.x265.WEBRip/Naruto.Shippuden.${episode.padStart(
-          3,
-          "0"
-        )}.1080p.x265.WEBRip.AnimeRG.DonyayeSerial.mkv`,
-        behaviorHints: {
-          bingeGroup: "1080p.x265.WEBRip.AnimeRG.DonyayeSerial",
-        },
-      },
-      {
-        title: "720p x265 mkv",
-        url: `${baseURL}${range[0]}-${
-          range[1]
-        }/720p.x265/Naruto.Shippuden.${episode.padStart(
-          3,
-          "0"
-        )}.720p.x265.DonyayeSerial.mkv`,
-        behaviorHints: {
-          bingeGroup: "720p.x265.DonyayeSerial",
-        },
-      },
-    ];
-  }
-
-  // The Simpsons
-  [series_id, season, episode] = id.split(":");
-  if (series_id === "tt0096697") {
-    const baseURL =
-      "https://img5.downloadha.com/hosein/Animation/October2019/The.Simpsons/";
-    if (season < 13) {
-      const res = await got(baseURL + `S${season.padStart(2, "0")}/`);
-      const $ = cheerio.load(res.body);
-      $("a").each((i, elem) => {
-        const link = elem.attribs.href;
-        if (
-          link.split(".")[2] ===
-          `S${season.padStart(2, "0")}E${episode.padStart(2, "0")}`
-        ) {
-          streams = [
+    // Naruto Shippuden
+    [prefix, series, episode] = id.split(":");
+    if (prefix === "kitsu" && series === "1555") {
+        const baseURL = `http://dls2.top-movies2filmha.tk/DonyayeSerial/series/Naruto.Shippuuden/`;
+        const range = [
+            (Math.floor((episode - 1) / 50) * 50 + 1 + "").padStart(3, "0"),
+            (Math.floor((episode - 1) / 50) * 50 + 50 + "").padStart(3, "0"),
+        ];
+        streams = [
             {
-              title: "DVDRip-jlw mkv",
-              url: `${baseURL}S${season.padStart(2, "0")}/${link}`,
-              behaviorHints: {
-                bingeGroup: `The.Simpsons.S${season.padStart(2, "0")}`,
-              },
+                title: "1080p x265 WEBRip AnimeRG mkv",
+                url: `${baseURL}${range[0]}-${
+                    range[1]
+                }/1080p.x265.WEBRip/Naruto.Shippuden.${episode.padStart(
+                    3,
+                    "0"
+                )}.1080p.x265.WEBRip.AnimeRG.DonyayeSerial.mkv`,
+                behaviorHints: {
+                    bingeGroup: "1080p.x265.WEBRip.AnimeRG.DonyayeSerial",
+                },
             },
-          ];
-        }
-      });
+            {
+                title: "720p x265 mkv",
+                url: `${baseURL}${range[0]}-${
+                    range[1]
+                }/720p.x265/Naruto.Shippuden.${episode.padStart(
+                    3,
+                    "0"
+                )}.720p.x265.DonyayeSerial.mkv`,
+                behaviorHints: {
+                    bingeGroup: "720p.x265.DonyayeSerial",
+                },
+            },
+        ];
     }
-  }
-  console.log(streams);
-  return Promise.resolve(streams);
+
+    // The Simpsons
+    [series_id, season, episode] = id.split(":");
+    if (series_id === "tt0096697") {
+        const baseURL =
+            "https://img5.downloadha.com/hosein/Animation/October2019/The.Simpsons/";
+        if (season < 13) {
+            const res = await got(baseURL + `S${season.padStart(2, "0")}/`);
+            const $ = cheerio.load(res.body);
+            $("a").each((i, elem) => {
+                const link = elem.attribs.href;
+                if (
+                    link.split(".")[2] ===
+                    `S${season.padStart(2, "0")}E${episode.padStart(2, "0")}`
+                ) {
+                    streams = [
+                        {
+                            title: "DVDRip-jlw mkv",
+                            url: `${baseURL}S${season.padStart(
+                                2,
+                                "0"
+                            )}/${link}`,
+                            behaviorHints: {
+                                bingeGroup: `The.Simpsons.S${season.padStart(
+                                    2,
+                                    "0"
+                                )}`,
+                            },
+                        },
+                    ];
+                }
+            });
+        }
+    }
+    console.log(streams);
+    return Promise.resolve(streams);
 };
 
-getStreams("tt0096697:5:1");
+const getAlmasMovieStreams = async function (id) {
+    let streams = [];
+    [series_id, season, episode] = id.split(":");
+    const baseURL = "http://iamnotindangeriamthedanger.website/filmgir/?i=";
+    for (let q = 1; q < 2; q++) {
+        const res = await got(baseURL + `${series_id}&f=${season}&q=${q}`);
+        const $ = cheerio.load(res.body);
+        const title = $("div.mb-2").text();
+        if (title) {
+            console.log(title);
+            $("a").each((i, elem) => {
+                if (i  == (episode -1) * 2) {
+                    streams.push({
+                        title: title,
+                        url: `${elem.attribs.href}`,
+                        behaviorHints: {
+                            bingeGroup: series_id + " " + title,
+                        },
+                    });
+                }
+            });
+        }
+    }
+    console.log(streams);
+    return Promise.resolve(streams);
+
+};
+
+getAlmasMovieStreams("tt9253284:1:5");
