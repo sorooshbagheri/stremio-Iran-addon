03fa1873db352ef07cc74f95a3b171bbf93c6ff4
sorooshbagheri <sorooshbagheri@outlook.com>
2023-07-18 01:14:47 +0330

    updated donyaye serial address again

--- addon.js
+++ addon.js
@@ -307,7 +307,7 @@ const recursiveAddStreams = async function (
 
 const getDonyayeSerialStreams = async function (id) {
     const searchURL =
-        "https://donyayeserial.website/wp-admin/admin-ajax.php?action=live_func";
+        "https://donyayeserial.pw/wp-admin/admin-ajax.php?action=live_func";
     let streams = [];
     [series_id, season, episode] = id.split(":");
     if (series_id == "kitsu") {
